1c86f56b3366d4531e6a37056d4392a59cc71603
DESKTOP-CSVUBT7\Yuda <meyda.aulia2@gmail.com>
2024-02-03 11:07:23 +0700

    view datatable, store and delete

---
+++ resources/views/contents/pengajuan/list.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@php
+    $plugins = ['datatable', 'swal', 'select2'];
+@endphp
+
+@section('contents')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    @if (rbacCheck('pengajuan', 2))
+                        <div class="row mb-2">
+                            <div class="col-sm-12">
+                                <div class="text-sm-right">
+                                    <button type="button"
+                                        class="btn btn-success btn-rounded waves-effect waves-light btn-tambah"><i
+                                            class="bx bx-plus-circle mr-1"></i> Tambah</button>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    <div class="table-responsive" data-pattern="priority-columns">
+                        <table class="table table-striped" id="table-data" style="width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th style="width: 5%;">No</th>
+                                    <th>Nama Lengkap</th>
+                                    <th>Pekerjaan</th>
+                                    <th>Tanggal Pengajuan</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('js/page/pengajuan/list.js?q=' . Str::random(5)) }}"></script>
+@endpush
