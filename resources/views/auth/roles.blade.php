413f20ece68159605fb301eee32d42836d55644e
heru10 <setiawanheru10@gmail.com>
2024-02-16 13:51:32 +0700

    clean

--- resources/views/auth/roles.blade.php
+++ resources/views/auth/roles.blade.php
@@ -4,10 +4,10 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Login | Skote - Responsive Bootstrap 4 Admin Dashboard</title>
+    <title>Login | Pilih Hak Akses</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Sistem Pertanahan" name="description" />
+    <meta content="Phicos" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('img/logo-kota-surakarta.png') }}">
 
