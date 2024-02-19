03d0217f1b3ce49e4b2f0acbebbc9e1153222c2b
reihnxx <reihanhafidz2@gmail.com>
2024-02-19 10:03:30 +0700

    route tanah

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -56,8 +56,10 @@ class RouteServiceProvider extends ServiceProvider
             $this->mapOtoritasRoutes();
 
             $this->mapPengajuanRoutes();
-            
+
             $this->mapTimLapanganRoutes();
+            
+            $this->mapPertanahanRoutes();
         });
     }
 
@@ -129,5 +131,12 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::prefix('timlapangan')
             ->group(base_path('routes/panel/timlapangan.php'));
+    }
+
+    protected function mapPertanahanRoutes()
+    {
+        Route::prefix('pertanahan')
+            // ->namespace($this->namespace)
+            ->group(base_path('routes/panel/pertanahan.php'));
     }
 }
